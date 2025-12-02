268e046e714672dfdebc1c760ac258e84397e229
DevWedeloper <vicnathangabrielle@gmail.com>
2025-12-03 01:58:27 +0800

    WIP

--- apps/angular/src/app/types/state.ts
+++ apps/angular/src/app/types/state.ts
@@ -1,21 +1,3 @@
 import { CountryCode } from 'libphonenumber-js'
 
 export type Mode = 'auto' | 'international' | 'national'
-
-interface BasePhoneState {
-  input: string
-}
-
-interface PhoneStateAuto extends BasePhoneState {
-  mode: 'auto'
-  country: CountryCode | null
-  countryContext: 'none' | 'user-selected' | 'auto-detected' | 'provided'
-}
-
-interface PhoneStateWithCountry extends BasePhoneState {
-  mode: 'international' | 'national'
-  country: CountryCode
-  countryContext: 'provided'
-}
-
-export type PhoneState = PhoneStateAuto | PhoneStateWithCountry // TODO: might remove this if im not using it
