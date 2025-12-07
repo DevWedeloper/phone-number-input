bed6750d5f4d4055368a21f52a55cac90abd1085
DevWedeloper <vicnathangabrielle@gmail.com>
2025-12-07 21:51:20 +0800

    WIP

--- packages/angular/projects/ngx-phone-number-input/src/test-setup.ts
+++ packages/angular/projects/ngx-phone-number-input/src/test-setup.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing'
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing'
 import '@angular/compiler'
 import '@analogjs/vitest-angular/setup-zone'
+import '@testing-library/jest-dom/vitest'
 
 TestBed.initTestEnvironment(
   [BrowserDynamicTestingModule],
