717f3c9db701774418f0ad65c0ac4ac54b7f3c25
DevWedeloper <vicnathangabrielle@gmail.com>
2026-01-19 17:16:56 +0800

    WIP

--- apps/docs/eslint.config.mjs
+++ apps/docs/eslint.config.mjs
@@ -1,5 +1,6 @@
 import antfu from '@antfu/eslint-config'
 import tsParser from '@typescript-eslint/parser'
+import eslintParserAstro from 'astro-eslint-parser'
 import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
 export default antfu({
@@ -9,6 +10,14 @@ export default antfu({
   files: ['**/*.ts', '**/*.tsx', '**/*.jsx'],
   languageOptions: {
     parser: tsParser,
+  },
+}, {
+  files: ['**/*.astro'],
+  languageOptions: {
+    parser: eslintParserAstro,
+  },
+  rules: {
+    'style/multiline-ternary': 'off',
   },
 }, {
   plugins: {
