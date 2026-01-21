00a1511d1da278b815544fb5e2ff2cf063293e75
DevWedeloper <vicnathangabrielle@gmail.com>
2026-01-21 23:33:58 +0800

    WIP

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     ssr: {
-      noExternal: ['@maskito/angular'],
+      noExternal: ['@maskito/angular', 'zod'],
     },
   },
   integrations: [
